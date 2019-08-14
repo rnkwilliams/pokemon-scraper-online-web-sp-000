c996fc0764d557c37233d18312917921c13ab4d8
Ranika Williams <rnkprice07@gmail.com>
2019-08-14 00:22:12 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,12 +15,12 @@ def self.find(num, db)
   new_pokemon.id = pokemon[0][0]
   new_pokemon.name = pokemon[0][1]
   new_pokemon.type = pokemon[0][2]
-  new_pokemon.hp = pokemon[0][3]
+  #new_pokemon.hp = pokemon[0][3]
   return new_pokemon
 end
 
-def alter_hp(num, db)
-  db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
-  self.hp = num
-end
+#def alter_hp(num, db)
+  #db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
+  #self.hp = num
+#end
 end
