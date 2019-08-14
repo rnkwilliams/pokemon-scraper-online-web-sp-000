88d55e5c90ca5e25e452bfc7ad1aee841aee9cb1
Ranika Williams <rnkprice07@gmail.com>
2019-08-14 00:22:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,8 +15,7 @@ def self.find(num, db)
   new_pokemon.id = pokemon[0][0]
   new_pokemon.name = pokemon[0][1]
   new_pokemon.type = pokemon[0][2]
-  #new_pokemon.hp = pokemon[0][3]
-  return new_pokemon
+  new_pokemon
 end
 
 #def alter_hp(num, db)
